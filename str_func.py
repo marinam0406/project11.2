b61a5e0b8900da858b7387a5f5e494de2abc3453
marinam0406 <marinam0406@yandex.ru>
2023-12-25 20:29:15 +0300

    add title_string

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def upper_string(string):
+    """
+    Функция, возвращает строку большими буквами
+    :param string: строка
+    :return: строку с большими буквами
+    """
     return string.upper()
+
+
+def title_string(string):
+    """
+    Функция, возвращает слова с заглавной буквы
+    :param string: строка
+    :return: строку со словами с большой буквы
+    """
+    return string.title()
